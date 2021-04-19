f2b8a6b35a1b08857786df0a6bba3f28a02c3c28
Diego <diemolina@gmail.com>
2021-04-19 01:09:56 -0300

    Sending functions

--- fns.js
+++ fns.js
@@ -12,15 +12,30 @@ function sum(a, b) {
 
 // Completar estas funciones
 
-function res(a, b) {}
+function res(a, b) {
+    return a - b;
+}
 
-function mul(a, b) {}
+function mul(a, b) {
+    return a * b;
+}
 
 function div(a, b) {
     // Tener en cuenta division por 0
-    // Si b es 0 retornar "Error: div by 0"
+    if (b != 0){
+       //return a / b;
+       console.log("working fine");
+       return a / b;
+    } else {
+    // Si b es 0 retornar "Error: div by 0"   
+       console.log("Error: div by 0"); 
+       return 0 
+    }
+    
 }
 
-function pow(a, b) {}
+function pow(a, b) {
+    return Math.pow(a, b);
+}
 
 module.exports = fns;
